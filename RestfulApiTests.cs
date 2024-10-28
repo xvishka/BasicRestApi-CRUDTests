a332401e433fda3dd35d9bd8a59036f245d7c6af
Kavindu Avishka <kxvindu@gmail.com>
2024-10-28 09:33:51 +0530

    Update RestfulApiTests.cs

--- RestfulApiTests.cs
+++ RestfulApiTests.cs
@@ -4,7 +4,6 @@ using System.Net;
 using System.Text.Json;
 using NUnit.Framework;
 using RestSharp;
-using Serilog;
 
 namespace RestfulApiValidation
 {
@@ -14,86 +13,113 @@ namespace RestfulApiValidation
         private readonly string baseUrl = "https://api.restful-api.dev/";
         private RestClient client;
         private string objectId;
-        private readonly ILogger log = new LoggerConfiguration()
-                                           .WriteTo.Console()
-                                           .CreateLogger();
 
         [SetUp]
         public void Setup()
         {
             client = new RestClient(baseUrl);
-            log.Information("Client setup with base URL: {BaseUrl}", baseUrl);
         }
 
-        [Test]
+        [Test,Order(1)]
         public void Test_GetAllObjects()
         {
             var request = new RestRequest("objects", Method.Get);
             var response = client.Execute(request);
 
-            log.Information("Status Code: {StatusCode}", response.StatusCode);
-            log.Information("Response Content: {Content}", response.Content);
+            Console.WriteLine($"Status Code: {response.StatusCode}");
+            Console.WriteLine($"Response Content: {response.Content}");
 
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
             Assert.IsNotEmpty(response.Content, "Expected content not to be empty");
         }
 
-        [Test] 
-        public void Test_CreateObject()
+
+        [Test,Order(3)]
+        public void Test_GetSingleObject()
         {
-            var request = new RestRequest("objects", Method.Post);
-            request.AddJsonBody(new 
-        { 
-            name = "Apple MacBook Pro 16",
-            data = new 
-        {
-            year = 2019,
-            price = 1849.99,
-            CPUmodel = "Intel Core i9",
-            HardDiskSize = "1 TB"
-        }
-    });
-
-        var response = client.Execute(request);
-        log.Information("Status Code: {StatusCode}", response.StatusCode);
-        log.Information("Response Content: {Content}", response.Content);
-
-        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-    
-        var responseData = JsonSerializer.Deserialize<Dictionary<string, object>>(response.Content);
-    
-
-        // Store objectId for use in other tests
-        objectId = responseData["id"].ToString();
-}
-
-
-        [Test] 
-        public void Test_GetSingleObject_ShouldReturnOkStatus()
-        {
-            if (objectId == null) Assert.Fail("objectId not set. Run Test_CreateObject first.");
-            
             var request = new RestRequest($"objects/{objectId}", Method.Get);
             var response = client.Execute(request);
 
-            log.Information("Status Code: {StatusCode}", response.StatusCode);
-            log.Information("Response Content: {Content}", response.Content);
+            Console.WriteLine($"Status Code: {response.StatusCode}");
+            Console.WriteLine($"Response Content: {response.Content}");
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.IsNotEmpty(response.Content, "Expected content not to be empty");
+        }
+
+        [Test,Order(2)]
+        public void Test_AddObject()
+        {
+            var request = new RestRequest("objects", Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+
+            request.AddJsonBody(new
+            {
+                name = "Sample Object",
+                data = new
+                {
+                    year = 2023,
+                    price = 999.99,
+                    CPU_model = "Intel Core i7",
+                    Hard_disk_size = "512 GB"
+                }
+            });
+
+            var response = client.Execute(request);
+
+            Console.WriteLine($"Status Code: {response.StatusCode}");
+            Console.WriteLine($"Response Content: {response.Content}");
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+            var jsonResponse = JsonSerializer.Deserialize<Dictionary<string, object>>(response.Content);
+            if (jsonResponse != null && jsonResponse.ContainsKey("id"))
+            {
+                objectId = jsonResponse["id"].ToString();
+            }
+            else
+            {
+                Assert.Fail("Failed to retrieve 'id' from the response content.");
+            }
+        }
+
+
+        [Test,Order(4)]
+        public void Test_UpdateObject()
+        {
+            var request = new RestRequest($"objects/{objectId}", Method.Put);
+            request.AddJsonBody(new
+            {
+                name = "Apple MacBook Pro 16",
+                data = new
+                {
+                    year = 2019,
+                    price = 2049.99,
+                    CPUmodel = "Intel Core i9",
+                    HardDiskSize = "1 TB",
+                    color = "silver"
+                }
+            });
+
+            var response = client.Execute(request);
+
+            Console.WriteLine($"Status Code: {response.StatusCode}");
+            Console.WriteLine($"Response Content: {response.Content}");
 
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
 
-        [Test] //tested ok
-        public void Test_DeleteObject_ShouldReturnNoContentStatus()
+
+        [Test,Order(5)]
+        public void Test_DeleteObject()
         {
-            if (objectId == null) Assert.Fail("objectId not set. Run Test_CreateObject first.");
-            
             var request = new RestRequest($"objects/{objectId}", Method.Delete);
             var response = client.Execute(request);
 
-            log.Information("Status Code: {StatusCode}", response.StatusCode);
-            log.Information("Response Content: {Content}", response.Content);
+            Console.WriteLine($"Status Code: {response.StatusCode}");
+            Console.WriteLine($"Response Content: {response.Content}");
 
-            Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
     }
 }
